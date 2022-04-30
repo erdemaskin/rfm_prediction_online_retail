617c5ce89886dd472ca71018083a72902a27f048
erdemaskin <79326273+erdemaskin@users.noreply.github.com>
2022-04-30 14:58:09 +0100

    Add files via upload

---
+++ rfm_prediction_online_retail_github.py
@@ -0,0 +1,338 @@
+import datetime as dt
+import pandas as pd
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+df_ = pd.read_excel("week_3/online_retail_II.xlsx", sheet_name="Year 2010-2011")
+df = df_.copy()
+df.head()
+df.shape
+df.isnull().sum()
+
+df["Description"].nunique()
+
+df["Description"].value_counts().head()
+
+df.groupby("Description").agg({"Quantity": "sum"}).head()
+
+df.groupby("Description").agg({"Quantity": "sum"}).sort_values("Quantity", ascending=False).head()
+
+df["Invoice"].nunique()
+
+df["TotalPrice"] = df["Quantity"] * df["Price"]
+
+df.groupby("Invoice").agg({"TotalPrice": "sum"}).head()
+
+
+###############################################################
+# 3. Veri Hazırlama (Data Preparation)
+###############################################################
+
+df.shape
+df.isnull().sum()
+df.describe().T
+df = df[(df['Quantity'] > 0)]
+df.dropna(inplace=True)
+df = df[~df["Invoice"].astype(str).str.contains("C", na=False)]
+
+
+###############################################################
+# 4. RFM Metriklerinin Hesaplanması (Calculating RFM Metrics)
+###############################################################
+
+# Recency, Frequency, Monetary
+
+df.head()
+
+df["InvoiceDate"].max()
+
+today_date = dt.datetime(2011, 12, 11)
+type(today_date)
+
+
+rfm = df.groupby('Customer ID').agg({'InvoiceDate': lambda InvoiceDate: (today_date - InvoiceDate.max()).days,
+                                     'Invoice': lambda Invoice: Invoice.nunique(),
+                                     'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+rfm.head()
+
+rfm.columns = ['recency', 'frequency', 'monetary']
+
+rfm.describe().T
+
+rfm = rfm[rfm["monetary"] > 0]
+rfm.shape
+
+###############################################################
+# 5. RFM Skorlarının Hesaplanması (Calculating RFM Scores)
+###############################################################
+
+rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+
+# 0-100, 0-20, 20-40, 40-60, 60-80, 80-100
+
+rfm["frequency_score"] = pd.qcut(rfm['frequency'].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+
+rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
+
+rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
+                    rfm['frequency_score'].astype(str))
+
+rfm.describe().T
+
+rfm[rfm["RFM_SCORE"] == "55"]
+
+rfm[rfm["RFM_SCORE"] == "11"]
+
+
+
+###############################################################
+# 6. RFM Segmentlerinin Oluşturulması ve Analiz Edilmesi (Creating & Analysing RFM Segments)
+###############################################################
+# regex
+
+# RFM isimlendirmesi
+
+
+seg_map = {
+    r'[1-2][1-2]': 'hibernating',
+    r'[1-2][3-4]': 'at_Risk',
+    r'[1-2]5': 'cant_loose',
+    r'3[1-2]': 'about_to_sleep',
+    r'33': 'need_attention',
+    r'[3-4][4-5]': 'loyal_customers',
+    r'41': 'promising',
+    r'51': 'new_customers',
+    r'[4-5][2-3]': 'potential_loyalists',
+    r'5[4-5]': 'champions'
+}
+
+rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
+
+rfm[["segment", "recency", "frequency", "monetary"]].groupby("segment").agg(["mean", "count"])
+
+rfm[rfm["segment"] == "cant_loose"].head()
+rfm[rfm["segment"] == "cant_loose"].index
+
+new_df = pd.DataFrame()
+new_df["new_customer_id"] = rfm[rfm["segment"] == "new_customers"].index
+
+new_df["new_customer_id"] = new_df["new_customer_id"].astype(int)
+
+new_df.to_csv("new_customers.csv")
+
+rfm.to_csv("rfm.csv")
+
+
+################################################ ###############
+# 7. Functionalizing the Whole Process
+################################################ ###############
+
+def create_rfm(dataframe, csv=False):
+
+    # PREPARING THE DATA
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+
+    # CALCULATION OF RFM metrics
+    today_date = dt.datetime(2011, 12, 11)
+    rfm = dataframe.groupby('Customer ID').agg({'InvoiceDate': lambda date: (today_date - date.max()).days,
+                                                'Invoice': lambda num: num.nunique(),
+                                                "TotalPrice": lambda price: price.sum()})
+    rfm.columns = ['recency', 'frequency', "monetary"]
+    rfm = rfm[(rfm['monetary'] > 0)]
+
+    # CALCULATION OF RFM SCORES
+    rfm["recency_score"] = pd.qcut(rfm['recency'], 5, labels=[5, 4, 3, 2, 1])
+    rfm["frequency_score"] = pd.qcut(rfm["frequency"].rank(method="first"), 5, labels=[1, 2, 3, 4, 5])
+    rfm["monetary_score"] = pd.qcut(rfm['monetary'], 5, labels=[1, 2, 3, 4, 5])
+
+    # cltv_df scores converted to categorical value and added to df
+    rfm["RFM_SCORE"] = (rfm['recency_score'].astype(str) +
+                        rfm['frequency_score'].astype(str))
+
+
+    # NAMING OF SEGMENTS
+    seg_map = {
+        r'[1-2][1-2]': 'hibernating',
+        r'[1-2][3-4]': 'at_risk',
+        r'[1-2]5': 'cant_loose',
+        r'3[1-2]': 'about_to_sleep',
+        r'33': 'need_attention',
+        r'[3-4][4-5]': 'loyal_customers',
+        r'41': 'promising',
+        r'51': 'new_customers',
+        r'[4-5][2-3]': 'potential_loyalists',
+        r'5[4-5]': 'champions'
+    }
+
+    rfm['segment'] = rfm['RFM_SCORE'].replace(seg_map, regex=True)
+    rfm = rfm[["recency", "frequency", "monetary", "segment"]]
+    rfm.index = rfm.index.astype(int)
+
+    if csv:
+        rfm.to_csv("rfm.csv")
+
+    return rfm
+
+df = df_.copy()
+
+rfm_new = create_rfm(df, csv=True)
+
+##################################
+#cltv predictions
+##################################
+
+##########################
+# Required Library and Functions
+##########################
+
+# !pip install lifetimes
+import datetime as dt
+import pandas as pd
+import matplotlib.pyplot as plt
+from lifetimes import BetaGeoFitter
+from lifetimes import GammaGammaFitter
+from lifetimes.plotting import plot_period_transactions
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+pd.set_option('display.float_format', lambda x: '%.4f' % x)
+from sklearn.preprocessing import MinMaxScaler
+
+
+def outlier_thresholds(dataframe, variable):
+    quartile1 = dataframe[variable].quantile(0.01)
+    quartile3 = dataframe[variable].quantile(0.99)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    # dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+
+
+
+
+def create_cltv_c(dataframe, profit=0.10):
+
+    # Preparing the data
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[(dataframe['Quantity'] > 0)]
+    dataframe.dropna(inplace=True)
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    cltv_c = dataframe.groupby('Customer ID').agg({'Invoice': lambda x: x.nunique(),
+                                                   'Quantity': lambda x: x.sum(),
+                                                   'TotalPrice': lambda x: x.sum()})
+    cltv_c.columns = ['total_transaction', 'total_unit', 'total_price']
+    # avg_order_value
+    cltv_c['avg_order_value'] = cltv_c['total_price'] / cltv_c['total_transaction']
+    # purchase_frequency
+    cltv_c["purchase_frequency"] = cltv_c['total_transaction'] / cltv_c.shape[0]
+    # repeat rate & churn rate
+    repeat_rate = cltv_c[cltv_c.total_transaction > 1].shape[0] / cltv_c.shape[0]
+    churn_rate = 1 - repeat_rate
+    # profit_margin
+    cltv_c['profit_margin'] = cltv_c['total_price'] * profit
+    # Customer Value
+    cltv_c['customer_value'] = (cltv_c['avg_order_value'] * cltv_c["purchase_frequency"])
+    # Customer Lifetime Value
+    cltv_c['cltv'] = (cltv_c['customer_value'] / churn_rate) * cltv_c['profit_margin']
+    # Segment
+    cltv_c["segment"] = pd.qcut(cltv_c["cltv"], 4, labels=["D", "C", "B", "A"])
+
+    return cltv_c
+
+df = df_.copy()
+
+clv = create_cltv_c(df)
+
+
+clv.head()
+clv.describe().T
+
+##################################
+#cltv prediction
+##################################
+
+def create_cltv_p(dataframe, month=3):
+    # 1. Data Preprocessing
+    dataframe.dropna(inplace=True)
+    dataframe = dataframe[~dataframe["Invoice"].str.contains("C", na=False)]
+    dataframe = dataframe[dataframe["Quantity"] > 0]
+    dataframe = dataframe[dataframe["Price"] > 0]
+    replace_with_thresholds(dataframe, "Quantity")
+    replace_with_thresholds(dataframe, "Price")
+    dataframe["TotalPrice"] = dataframe["Quantity"] * dataframe["Price"]
+    today_date = dt.datetime(2011, 12, 11)
+
+    cltv_df = dataframe.groupby('Customer ID').agg(
+        {'InvoiceDate': [lambda InvoiceDate: (InvoiceDate.max() - InvoiceDate.min()).days,
+                         lambda InvoiceDate: (today_date - InvoiceDate.min()).days],
+         'Invoice': lambda Invoice: Invoice.nunique(),
+         'TotalPrice': lambda TotalPrice: TotalPrice.sum()})
+
+    cltv_df.columns = cltv_df.columns.droplevel(0)
+    cltv_df.columns = ['recency', 'T', 'frequency', 'monetary']
+    cltv_df["monetary"] = cltv_df["monetary"] / cltv_df["frequency"]
+    cltv_df = cltv_df[(cltv_df['frequency'] > 1)]
+    cltv_df["recency"] = cltv_df["recency"] / 7
+    cltv_df["T"] = cltv_df["T"] / 7
+
+    # 2. Establishment of BG-NBD Model
+    bgf = BetaGeoFitter(penalizer_coef=0.001)
+    bgf.fit(cltv_df['frequency'],
+            cltv_df['recency'],
+            cltv_df['T'])
+
+    cltv_df["expected_purc_1_week"] = bgf.predict(1,
+                                                  cltv_df['frequency'],
+                                                  cltv_df['recency'],
+                                                  cltv_df['T'])
+
+    cltv_df["expected_purc_1_month"] = bgf.predict(4,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+    cltv_df["expected_purc_3_month"] = bgf.predict(12,
+                                                   cltv_df['frequency'],
+                                                   cltv_df['recency'],
+                                                   cltv_df['T'])
+
+    # 3. Establishing the GAMMA-GAMMA Model
+    ggf = GammaGammaFitter(penalizer_coef=0.01)
+    ggf.fit(cltv_df['frequency'], cltv_df['monetary'])
+    cltv_df["expected_average_profit"] = ggf.conditional_expected_average_profit(cltv_df['frequency'],
+                                                                                 cltv_df['monetary'])
+
+    # 4. Calculation of CLTV with BG-NBD and GG model.
+    cltv = ggf.customer_lifetime_value(bgf,
+                                       cltv_df['frequency'],
+                                       cltv_df['recency'],
+                                       cltv_df['T'],
+                                       cltv_df['monetary'],
+                                       time=month,  # 3 aylık
+                                       freq="W",  # T'nin frekans bilgisi.
+                                       discount_rate=0.01)
+
+    cltv = cltv.reset_index()
+    cltv_final = cltv_df.merge(cltv, on="Customer ID", how="left")
+    cltv_final["segment"] = pd.qcut(cltv_final["clv"], 4, labels=["D", "C", "B", "A"])
+
+    return cltv_final
+
+
+df = df_.copy()
+
+cltv_final2 = create_cltv_p(df)
+
+cltv_final2.to_csv("cltv_prediction.csv")
+
+create_cltv_p(df, month=3)
